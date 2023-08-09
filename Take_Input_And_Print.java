122c13540e17f9bd05e63aed825a2572958d186b
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-08-09 19:34:53 +0530

    import java.util.Scanner; public class P{     public static void main(String[] args){         Scanner s=new Scanner(System.in);         String n=s.nextLine();         System.out.println(n);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class P{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        String n=s.nextLine();
+        System.out.println(n);
+    }
+}
