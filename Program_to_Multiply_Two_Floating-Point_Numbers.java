7a958eed397593aee1379543e854df61b9b9998a
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-08-11 09:49:36 +0530

    import java.util.*; public class Div{     public static void main(String[] args){         Scanner s=new Scanner(System.in);         double a=s.nextDouble();         double b=s.nextDouble();         System.out.printf("%.2f",a*b);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Div{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        double a=s.nextDouble();
+        double b=s.nextDouble();
+        System.out.printf("%.2f",a*b);
+    }
+}
