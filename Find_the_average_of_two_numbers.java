fcb3cebedc53a2cd88130e2b51568601127bfd1e
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-08-12 09:51:57 +0530

    import java.util.*; public class Average{     public static void main(String[] args){         Scanner s=new Scanner(System.in);         int a=s.nextInt();         int b=s.nextInt();         double c= (a+b)/2.0;         System.out.printf("%.4f",c);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Average{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        double c= (a+b)/2.0;
+        System.out.printf("%.4f",c);
+    }
+}
