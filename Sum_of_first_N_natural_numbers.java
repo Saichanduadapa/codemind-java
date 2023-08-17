dc4004eda7b8651797ed6532ce91c0c3776a7b7c
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-08-17 11:07:17 +0530

    import java.util.*; public class Sum{     public static void main(String[] args){         Scanner s=new Scanner(System.in);         int a=s.nextInt();         int n=(a*(a+1))/2;         System.out.println(n);     } }

---
+++ Sum_of_first_N_natural_numbers.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Sum{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int n=(a*(a+1))/2;
+        System.out.println(n);
+    }
+}
