594fb5c4cb2da0d5a8fc9b520b4235aa44d75295
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-08-19 12:07:27 +0530

    import java.util.*; public class Print{     public static void main(String[] args){         Scanner s=new Scanner(System.in);         int n=s.nextInt();         for(int i=1;i<=n;i++){             for(int j=i;j<=n;j++){                 System.out.print(j+" ");             }             System.out.println();         }     } }

---
+++ Print_the_following_pattern_12.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Print{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=i;j<=n;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
