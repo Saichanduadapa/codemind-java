32072f06e625dc939cc40ac67054f50e6a6e25ef
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-08-25 10:17:02 +0530

    import java.util.*; public class Octal{     public static void main(String[] args){         Scanner s=new Scanner(System.in);         String n=s.next();         System.out.println(Integer.parseInt(n,8));
        }
    }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Octal{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        String n=s.next();
+        System.out.println(Integer.parseInt(n,8));
+        
+    }
+}
