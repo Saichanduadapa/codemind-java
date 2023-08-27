aba9905fe1ff61baa3134b5ad84ac3a0cb2939bd
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-08-27 18:47:04 +0530

    import java.util.*; public class Dis{     public static void main(String[] args){         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int[] arr=new int[n];         for(int i=0;i<n;i++){             arr[i]=s.nextInt();         }         int e=0;         int m=0;         for(int i=0;i<n;i++){             int c=0;             for(int j=0;j<n;j++){                 if (arr[i]==arr[j]){                     c+=1;                 }             }             if (c==1) {                 e+=1;                 if(m<arr[i]){                     m=arr[i];                     }             }         }         if (e==0) System.out.println(-1);         else System.out.println(m);     } }

---
+++ UNIQUE_MAXIMUM_NUMBER.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Dis{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=s.nextInt();
+        }
+        int e=0;
+        int m=0;
+        for(int i=0;i<n;i++){
+            int c=0;
+            for(int j=0;j<n;j++){
+                if (arr[i]==arr[j]){
+                    c+=1;
+                }
+            }
+            if (c==1) {
+                e+=1; 
+                if(m<arr[i]){
+                    m=arr[i];
+                    } 
+            }
+        }
+        if (e==0) System.out.println(-1);
+        else System.out.println(m);
+    }
+}
