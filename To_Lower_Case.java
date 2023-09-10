8fc66646b09dbe4292cb8b18ab1b308adf30150b
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-09-10 20:51:28 +0530

    import java.util.*; public class Low{     public static void main(String[] args){         Scanner s=new Scanner(System.in);         String str=s.nextLine();         String res=str.toLowerCase();         System.out.println(res);     } }

---
+++ To_Lower_Case.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Low{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        String str=s.nextLine();
+        String res=str.toLowerCase();
+        System.out.println(res);
+    }
+}
