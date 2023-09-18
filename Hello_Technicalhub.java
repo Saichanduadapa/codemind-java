08a5225955ee64579994cf5006c45bb890a5f3e4
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-09-18 20:39:13 +0530

    import java.util.*; public class Dig{     public static void main(String[] args){         Scanner s=new Scanner(System.in);         String str=s.nextLine();         System.out.println("Hello Technicalhub");         System.out.println(str);     } }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Dig{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        String str=s.nextLine();
+        System.out.println("Hello Technicalhub");
+        System.out.println(str);
+    }
+}
