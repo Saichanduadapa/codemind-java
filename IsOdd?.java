2a979c1befa6205377b6c1b35b0aaacb5e617f63
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-09-30 21:31:39 +0530

    import java.util.*; public class Odd{     public static void main(String[] args){         Scanner s=new Scanner(System.in);         int n=s.nextInt();         if (n%2==0)         System.out.println(1);         else         System.out.println(2);     } }

---
+++ IsOdd?.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Odd{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        if (n%2==0)
+        System.out.println(1);
+        else
+        System.out.println(2);
+    }
+}
