47297c8bfb85375e2b5128ff9028cbc335453e4a
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-10-19 20:58:01 +0530

    import java.util.*; public class Can{     public static void main(String[] args){         Scanner s=new Scanner(System.in);         int t=s.nextInt();         for(int i=0;i<t;i++){             int a=s.nextInt();             int b=s.nextInt();             if((b-a)<a && a!=b){                 System.out.println("NO");             }             else{                 System.out.println("YES");             }         }     } }

--- Can_be_a_factor?.java
+++ Can_be_a_factor?.java
@@ -1,17 +1,17 @@
 import java.util.*;
 public class Can{
-public static void main(String[] args){
-    Scanner s=new Scanner(System.in);
-    int n=s.nextInt();
-    for (int i=0;i<n;i++){
-        int a=s.nextInt();
-        int b=s.nextInt();
-        if((b-a)<a && a!=b){
-            System.out.println("NO");
-        }
-        else{
-            System.out.println("YES");
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int t=s.nextInt();
+        for(int i=0;i<t;i++){
+            int a=s.nextInt();
+            int b=s.nextInt();
+            if((b-a)<a && a!=b){
+                System.out.println("NO");
+            }
+            else{
+                System.out.println("YES");
+            }
         }
     }
-}
 }
