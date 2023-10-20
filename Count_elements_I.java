e8be632c254feacddfd4d30b88df3a5f66c92c14
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-10-20 10:25:32 +0530

    import java.util.*; public class Cnt{     public static void main(String[] args){         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int m=s.nextInt();         HashSet<Integer> s1=new HashSet<Integer>();         HashSet<Integer> s2=new HashSet<Integer>();         for(int i=0;i<n;i++){             s1.add(s.nextInt());         }         for(int i=0;i<m;i++){             s2.add(s.nextInt());         }         s1.retainAll(s2);         System.out.println(s1.size());     } }

--- Count_elements_I.java
+++ Count_elements_I.java
@@ -1,21 +1,18 @@
 import java.util.*;
 public class Cnt{
     public static void main(String[] args){
-        Scanner s=new Scanner (System.in);
+        Scanner s=new Scanner(System.in);
         int n=s.nextInt();
         int m=s.nextInt();
-        int c=0;
-        HashSet<Integer> ar1=new HashSet<Integer>();
-        HashSet<Integer> ar2=new HashSet<Integer>();
+        HashSet<Integer> s1=new HashSet<Integer>();
+        HashSet<Integer> s2=new HashSet<Integer>();
         for(int i=0;i<n;i++){
-            int a=s.nextInt();
-            ar1.add(a);
+            s1.add(s.nextInt());
         }
         for(int i=0;i<m;i++){
-            int a=s.nextInt();
-            ar2.add(a);
+            s2.add(s.nextInt());
         }
-        ar1.retainAll(ar2);
-        System.out.println(ar1.size());
+        s1.retainAll(s2);
+        System.out.println(s1.size());
     }
 }
