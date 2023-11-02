5921df39b0e173bb42c7da3768648ee60f7de629
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-11-02 21:22:30 +0530

    import java.util.*; public class Miss{     public static void main(String[] args){         Scanner s=new Scanner(System.in);         int t=s.nextInt();         for(int k=0;k<t;k++){         int n=s.nextInt();         int c=0;         ArrayList<Integer> ar=new ArrayList<Integer>();         for(int i=0;i<n-1;i++){             ar.add(s.nextInt());         }         for(int i=1;i<=n;i++){             boolean res=ar.contains(i);             if(res==true){                 c+=1;             }             else{                 System.out.println(i);             }         }         }     } }

---
+++ Missing_number_in_array.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Miss{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int t=s.nextInt();
+        for(int k=0;k<t;k++){
+        int n=s.nextInt();
+        int c=0;
+        ArrayList<Integer> ar=new ArrayList<Integer>();
+        for(int i=0;i<n-1;i++){
+            ar.add(s.nextInt());
+        }
+        for(int i=1;i<=n;i++){
+            boolean res=ar.contains(i);
+            if(res==true){
+                c+=1;
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+        }
+    }
+}
