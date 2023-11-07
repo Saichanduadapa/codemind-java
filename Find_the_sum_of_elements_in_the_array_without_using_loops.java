708d4ec562c7c5e09d4655a1761daf07a313429a
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2023-11-07 18:46:54 +0530

    import java.util.*; public class Sum{     public static void main(String[] args){         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int sum=0;         for(int i=0;i<n;i++){             sum+=s.nextInt();         }         System.out.println(sum);     } }

---
+++ Find_the_sum_of_elements_in_the_array_without_using_loops.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Sum{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=s.nextInt();
+        }
+        System.out.println(sum);
+    }
+}
