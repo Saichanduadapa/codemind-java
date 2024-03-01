494845e35abd1e5c3c969f581b7f70cb1b580a9f
Saichandu <119840787+Saichanduadapa@users.noreply.github.com>
2024-03-01 21:00:08 +0530

    import java.util.*; public class IP{ public static void main(String[] args){     Scanner sc=new Scanner(System.in);     String s=sc.nextLine();     for(int i=0;i<s.length();i++){         if (s.charAt(i)=='.'){             System.out.print("[.]");         }         else{             System.out.print(s.charAt(i));         }     } } }

---
+++ Defanging_an_IP_Address.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class IP{
+public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    String s=sc.nextLine();
+    for(int i=0;i<s.length();i++){
+        if (s.charAt(i)=='.'){
+            System.out.print("[.]");
+        }
+        else{
+            System.out.print(s.charAt(i));
+        }
+    }
+}
+}
